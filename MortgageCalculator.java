d3e02690337e2ac9ae17048611b76facece5250a
marvelousakinyemi910-commits <marvelousakinyemi910@gmail.com>
2025-12-25 17:56:18 +0100

    Mortgage Calculator

---
+++ MortgageCalculator.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class MortgageCalculator{
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the principal amount: ");
+        int principal = input.nextInt();
+        System.out.println("Enter the annual Interest rate: ");
+        double interest = input.nextDouble();
+        System.out.println("Enter the duration in years: ");
+        int duration = input.nextInt();
+
+        double rate = interest/(100*12);
+        duration = duration*12;
+        
+        double monthlyPayment = principal*rate*Math.pow(1+rate,duration)/(Math.pow(1+rate,duration)-1);
+        
+        System.out.printf("Your monthly payment is $%.2f%n",monthlyPayment);
+        
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+}
